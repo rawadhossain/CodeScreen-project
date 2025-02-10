b50230a7ba8502e580b014f613a5eac0a21425ad
rawadhossain <rawad.hossain00@gmail.com>
2025-02-10 23:43:40 +0600

    convex clerk stream setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: 'https://guided-hare-78.clerk.accounts.dev',
+            applicationID: 'convex',
+        },
+    ],
+};
---
+++ convex/schema.ts
@@ -0,0 +1,12 @@
+import { defineSchema, defineTable } from 'convex/server';
+import { v } from 'convex/values';
+
+export default defineSchema({
+    user: defineTable({
+        name: v.string(),
+        email: v.string(),
+        image: v.optional(v.string()),
+        role: v.union(v.literal('candidate'), v.literal('interviewer')), //allows a value to have multiple possible types
+        clerkId: v.string(),
+    }).index('by_clerk_id', ['clerkId']),
+});
